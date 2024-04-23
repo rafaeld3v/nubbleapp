2bd9f160dac6b275717a0481768815e431c197da
rafaeld3v <rafaelcontato.ce@gmail.com>
2024-04-23 17:44:56 -0300

    feat: add Text Component

--- App.tsx
+++ App.tsx
@@ -6,15 +6,9 @@ import {Text} from './src/components/Text/Text';
 function App(): React.JSX.Element {
   return (
     <SafeAreaView>
-      <Text preset="headingLarge" style={{fontFamily: 'Satoshi-Italic'}}>
-        rafaeld3v
-      </Text>
-      <Text preset="headingSmall" style={{fontFamily: 'Satoshi-Black'}}>
-        rafaeld3v
-      </Text>
-      <Text preset="paragraphMedium" style={{fontFamily: 'Satoshi-Regular'}}>
-        rafaeld3v
-      </Text>
+      <Text preset="headingLarge">rafaeld3v</Text>
+      <Text preset="headingSmall">rafaeld3v</Text>
+      <Text preset="paragraphMedium">rafaeld3v</Text>
     </SafeAreaView>
   );
 }
