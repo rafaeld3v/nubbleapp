7071fe14926059a9528871dd41157bc88561418d
rafaeld3v <rafaelcontato.ce@gmail.com>
2024-04-24 10:02:04 -0300

    feat: add ESLint + Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: "@react-native",
+  rules: {
+    bracketSpacing: true,
+    arrowParens: "avoid",
+    bracketSameLine: true,
+    singleQuote: false,
+    trailingComma: "all",
+  },
 };
