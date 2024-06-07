a330faa7233fa776d37ff88252d6a7aa00544d66
rafaeld3v <rafaelcontato.ce@gmail.com>
2024-06-07 16:43:10 -0300

    feat: add Componentes Complexos  - Home Screen FlatList

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -3,7 +3,7 @@ import { KeyboardAvoidingView, Platform } from "react-native";
 
 import { useNavigation } from "@react-navigation/native";
 
-import { Box, TouchableOpacityBox, Icon, Text } from "@components";
+import { Box, TouchableOpacityBox, Icon, Text, BoxProps } from "@components";
 import { useAppSafeArea, useAppTheme } from "@hooks";
 
 import {
@@ -11,7 +11,7 @@ import {
   ViewContainer,
 } from "./components/ScreenContainer";
 
-interface ScreenProps {
+interface ScreenProps extends BoxProps {
   children: React.ReactNode;
   canGoBack?: boolean;
   scrollable?: boolean;
@@ -21,6 +21,8 @@ export function Screen({
   children,
   canGoBack = false,
   scrollable = false,
+  style,
+  ...boxProps
 }: ScreenProps) {
   const { bottom, top } = useAppSafeArea();
   const { colors } = useAppTheme();
@@ -34,9 +36,9 @@ export function Screen({
       behavior={Platform.OS === "ios" ? "padding" : undefined}>
       <Container backgroundColor={colors.background}>
         <Box
-          paddingBottom="s24"
           paddingHorizontal="s24"
-          style={{ paddingTop: top, paddingBottom: bottom }}>
+          style={[{ paddingTop: top, paddingBottom: bottom }, style]}
+          {...boxProps}>
           {canGoBack && (
             <TouchableOpacityBox
               onPress={navigation.goBack}
