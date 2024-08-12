6d82d8e14a0b6ace6f4945e92b1e38adb6bae157
rafaeld3v <rafaelcontato.ce@gmail.com>
2024-08-12 08:59:29 -0300

    feat: add Toast Message - Estado Global com Context e Zustand

--- App.tsx
+++ App.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 
+// import {ToastProvider} from '@services';
 import { ThemeProvider } from "@shopify/restyle";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
+import { Toast } from "@components";
 import { Router } from "@routes";
 import { theme } from "@theme";
 
-function App(): React.JSX.Element {
+function App(): JSX.Element {
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
+        {/* Only use ToastProvider if it is using Context implementation.
+          Zustand implementation doesn't need a provider */}
+        {/* <ToastProvider> */}
         <Router />
+        <Toast />
+        {/* </ToastProvider> */}
       </ThemeProvider>
     </SafeAreaProvider>
   );
