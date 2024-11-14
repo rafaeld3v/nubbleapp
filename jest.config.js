390571cc8e2ec85e2a518205d6da7fdc405a8409
rafaeld3v <rafaelcontato.ce@gmail.com>
2024-11-14 11:14:30 -0300

    feat: add Teste de Componentes com a React Native Testing Library

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   preset: "react-native",
+  setupFiles: ["<rootDir>/src/test/jestSetup.ts"],
+  moduleDirectories: ["node_modules", "./src/test"],
+  modulePathIgnorePatterns: [".*/mockedData/.*"],
   collectCoverageFrom: ["src/{components,utils}/**/*.{js,jsx,ts,tsx}"],
   coveragePathIgnorePatterns: ["/node_modules/", "index"],
 };
